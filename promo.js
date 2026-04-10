97459bae9e3551d6b86b6f770025b8e457f91a16
Tkarrman <tkarrman@gmail.com>
2026-04-11 01:42:52 +0200

    Promo

---
+++ promo.js
@@ -0,0 +1,24 @@
+{
+  "games": [
+    {
+      "title": "Garden Sprites",
+      "description": "Relaxing puzzle gameplay.",
+      "imageUrl": "https://yourname.gitlab.io/more/images/garden-sprites.png",
+      "linkUrl": "https://yourname.gitlab.io/garden-sprites/"
+    },
+    {
+      "title": "Next Game",
+      "description": "Coming soon.",
+      "imageUrl": "https://yourname.gitlab.io/more/images/next-game.png",
+      "linkUrl": "https://yourname.gitlab.io/next-game/"
+    }
+  ],
+  "news": [
+    {
+      "title": "Update 1.2",
+      "description": "New levels and improvements.",
+      "imageUrl": "https://yourname.gitlab.io/more/images/update.png",
+      "linkUrl": "https://yourname.gitlab.io/more/news/update-12.html"
+    }
+  ]
+}
